e283c360085b535973632ff29f0aac1345d34534
yixiyotatsuki <161025930+yixiyotatsuki@users.noreply.github.com>
2024-04-17 22:32:35 +0800

    Update lib.lua

---
+++ lib.lua
@@ -0,0 +1,7 @@
+local workspace = game.Workspace or game:GetService("Workspace")
+local executor = identifyexecutor and identifyexecutor() or getexecutorname and getexecutorname()
+local lplr = game:GetService("Players").LocalPlayer or game.Players.LocalPlayer
+local gui = Instance.new("ScreenGui", lplr.PlayerGui)
+gui.ResetOnSpawn = false
+local owner = "umother2001vjd"
+print("executor: ", executor)
